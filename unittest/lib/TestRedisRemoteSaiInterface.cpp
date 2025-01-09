34ea88b9221694e8732724bbf8dd3e3047461b32
Stepan Blyschak <stepanb@nvidia.com>
2025-01-09 13:26:45 +0000

    [sairedis/syncd] Implement bulk get support
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- unittest/lib/TestRedisRemoteSaiInterface.cpp
+++ unittest/lib/TestRedisRemoteSaiInterface.cpp
@@ -4,27 +4,3 @@
 #include <gtest/gtest.h>
 
 using namespace sairedis;
-
-TEST(RedisRemoteSaiInterface, bulkGet)
-{
-    auto ctx = ContextConfigContainer::loadFromFile("foo");
-    auto rec = std::make_shared<Recorder>();
-
-    RedisRemoteSaiInterface sai(ctx->get(0), nullptr, rec);
-
-    sai_object_id_t oids[1] = {0};
-    uint32_t attrcount[1] = {0};
-    sai_attribute_t* attrs[1] = {0};
-    sai_status_t statuses[1] = {0};
-
-    EXPECT_EQ(SAI_STATUS_NOT_IMPLEMENTED,
-            sai.bulkGet(
-                SAI_OBJECT_TYPE_PORT,
-                1,
-                oids,
-                attrcount,
-                attrs,
-                SAI_BULK_OP_ERROR_MODE_STOP_ON_ERROR,
-                statuses));
-}
-
