e9489fb41c9b1f34f9b7cae87e466bea6a2a1862
Stepan Blyschak <stepanb@nvidia.com>
2025-01-10 13:55:06 +0000

    Don't support switch and scheduler group objects
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- syncd/Syncd.cpp
+++ syncd/Syncd.cpp
@@ -962,6 +962,19 @@ sai_status_t Syncd::processBulkQuadEventInInitViewMode(
 
         case SAI_COMMON_API_BULK_SET:
 
+            switch (objectType)
+            {
+                case SAI_OBJECT_TYPE_SWITCH:
+                case SAI_OBJECT_TYPE_SCHEDULER_GROUP:
+
+                    SWSS_LOG_THROW("%s is not supported in init view mode",
+                            sai_serialize_object_type(objectType).c_str());
+
+                default:
+
+                    break;
+            }
+
             sendApiResponse(api, SAI_STATUS_SUCCESS, (uint32_t)statuses.size(), statuses.data());
 
             syncUpdateRedisBulkQuadEvent(api, statuses, objectType, objectIds, strAttributes);
